f7dbe11b77eefa5a96e4d8c2f57a5eb84d69ab75
SimoN <semeckinanton@gmail.com>
2025-11-23 15:19:20 +0300

    component "users"

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Users } from './pages/users/users';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Users
   ],
   imports: [
     BrowserModule,
